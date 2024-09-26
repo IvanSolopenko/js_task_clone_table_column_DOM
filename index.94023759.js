842dd1a406517ac57c7352d004ddac08b1fcb28c
Ivan Solopenko <solopenko16@gmail.com>
2024-09-26 17:58:28 +0300

    make build

---
+++ index.94023759.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),r=e.rows[0].cells.length-1,l=0;l<e.rows.length;l++){var o=e.rows[l].cells[1].cloneNode(!0);e.rows[l].insertBefore(o,e.rows[l].cells[r])}
+//# sourceMappingURL=index.94023759.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+const lastColumnIndex = table.rows[0].cells.length - 1;
+for(let i = 0; i < table.rows.length; i++){
+    const cellToClone = table.rows[i].cells[1];
+    const clonedCell = cellToClone.cloneNode(true);
+    table.rows[i].insertBefore(clonedCell, table.rows[i].cells[lastColumnIndex]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
